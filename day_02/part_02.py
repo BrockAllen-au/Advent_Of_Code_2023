310d9c5c9c84ac068515b7f67f267bb03a10261d
BrockAllen-au <brock.allen61@gmail.com>
2023-12-21 19:09:31 +1000

    Add completed day 02 part 02

--- day_02/part_02.py
+++ day_02/part_02.py
@@ -1,14 +1,17 @@
+"""
+Part Two no longer checks if the game was impossible or not.
+Just check if dice number for the particular colour is greater than it's previous value for that game set.
+Iterate through each game set, multiply red, green blue.
+Add to list and sum the list to get final total.
+"""
 import re
 
 INPUT_FILE = 'input_file_1.txt'
 SEPARATORS = r'[:;]'
-MAX_RED = 12
-MAX_GREEN = 13
-MAX_BLUE = 14
 
 
 def main():
-    total = 0
+    dice_multiplied = []
 
     with open(INPUT_FILE) as in_file:
         lines = [line.strip() for line in in_file]
@@ -19,29 +22,30 @@ def main():
         game_line = [re.sub(r'^\s+|\s+$', '', part) for part in game_line]
         # Extract the game ID number as an int
         game_id = int(re.findall(r'(\d+)', game_line[0])[0])
-        game_valid = True
+        red = 0
+        green = 0
+        blue = 0
         # Start at index 1 as index 0 is the game_id
         for game in game_line[1:]:
-            red = 0
-            green = 0
-            blue = 0
             # Splits each colour draw
             colours_drawn = game.strip().split(',')
             for colour in colours_drawn:
                 # Splits the number and colour
                 individual_colour = colour.strip().split(' ')
                 if individual_colour[1] == 'red':
-                    red += int(individual_colour[0])
+                    if int(individual_colour[0]) > red:
+                        red = int(individual_colour[0])
                 elif individual_colour[1] == 'green':
-                    green += int(individual_colour[0])
+                    if int(individual_colour[0]) > green:
+                        green = int(individual_colour[0])
                 elif individual_colour[1] == 'blue':
-                    blue += int(individual_colour[0])
-                if red > MAX_RED or green > MAX_GREEN or blue > MAX_BLUE:
-                    game_valid = False
-        if game_valid:
-            total += game_id
+                    if int(individual_colour[0]) > blue:
+                        blue = int(individual_colour[0])
 
-    print(f"Game_ID Total: {total}")
+        dice_multiplied.append(red * green * blue)
+
+    total = sum(dice_multiplied)
+    print(f"Part 02 total: {total}")
 
 
 if __name__ == '__main__':
